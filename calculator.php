42cebcdf7b05305a95c893014210a834e11fafcf
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-03-01 14:47:40 +0700

    Initial commit

---
+++ calculator.php
@@ -0,0 +1,17 @@
+<?php
+class calculator{
+     public function multiply($num1,$num2){
+         return $num1 * $num2;
+     }
+     public function addNumber($num1,$num2){
+        return $num1 + $num2;
+    }
+    public function subtract($num1,$num2){
+        return $num1 - $num2;
+    }
+
+}
+
+
+
+?>
